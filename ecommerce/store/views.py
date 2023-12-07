9aed81ea63b262500010a9f97e8c2a8ddcbda27c
tech1 <user@email.com>
2023-12-07 09:28:45 +0100

    dodawanie faktycznych stron

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def store(req):
+    context = {}
+    return render(req, 'store/store.html', context)
+
+def cart(req):
+    context = {}
+    return render(req, 'store/cart.html', context)
+
+def checkout(req):
+    context = {}
+    return render(req, 'store/checkout.html', context)
