46f4891e327db33fe21ccdc9852fe457838aa78e
TymekBrunka <tymkopolo@wp.pl>
2024-02-22 08:50:24 +0100

    naprawiono nie możne wejśc do cart.html

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -38,13 +38,13 @@ class Order(models.Model):
     
     @property
     def get_cart_total(self):
-        orderitems = self.orderitems_set.all()
+        orderitems = self.orderitem_set.all()
         total = sum([ item.get_total for item in orderitems ])
         return total
     
     @property
     def get_cart_items(self):
-        orderitems = self.orderitems_set.all()
+        orderitems = self.orderitem_set.all()
         total = sum([item.quantity for item in orderitems])
 
 class OrderItem(models.Model):
